2c9063dfd88a47169d307a9600592810d80ab178
tiario <tiario2004@gmail.com>
2025-03-18 11:28:26 -0300

    streak

--- test.ts
+++ test.ts
@@ -6,6 +6,15 @@ async function testar() {
     try {
         const data = await PoliglotasService.buscarPoliglota("Thamy0137");
         console.log(data);
+
+        // Acessando os dados do streakData
+        const streakData = data.users[0].streakData.currentStreak;
+
+        // Exibindo as informações do streakData
+        console.log("Start Date:", streakData.startDate);
+        console.log("Streak Length:", streakData.length);
+        console.log("End Date:", streakData.endDate);
+
     } catch (error) {
         console.error("Erro no teste:", error);
     }

2e47fa232cd241d27c91832863fcc8673b78871a
endril18 <endril.claudino@dcx.ufpb.br>
2025-05-13 08:57:05 -0300

    alterando o type do package.json e adicionando Poliglota manualmente no types

--- backend/src/types/poliglota.ts
+++ backend/src/types/poliglota.ts
@@ -1,2 +1,15 @@
 
-export type DadosDuolingo = Omit<Poliglota, 'id'>; // para não usar o id
+// Definindo o tipo Poliglota manualmente, para evitar dependências externas,
+// garantir que o código funcione mesmo sem o import direto do Prisma
+
+export type Poliglota = {
+  id: number;
+  nome: string;
+  idiomas: string;
+  xp: number;
+  ofensiva: number;
+  ultimaAtividade: Date | null;
+};
+
+// DadosDuolingo omitindo o 'id' do tipo Poliglota
+export type DadosDuolingo = Omit<Poliglota, 'id'>;
